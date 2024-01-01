67f4805aa558c7cf582b2c98321ab1628511aef5
TeknisiPustik <yridho88@gmail.com>
2024-01-01 23:24:16 +0700

    first commit

---
+++ application/views/administrator/dashboard/index.php
@@ -0,0 +1,22 @@
+        <div class="row">
+			<ol class="breadcrumb">
+				<li><a href="<?=base_url('/administrator.html')?>"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+				<li class="active">Dashboard</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">
+                    <i class="fa fa-dashboard"></i>
+                    Selamat Datang <?=$this->session->userdata('administrator')['nama']?>!!! | Repost by <a href='https://shopee.co.id/kokoqory' title='Kokoqory' target='_blank'>Kokoqory</a>
+					
+                </h1>
+			</div>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-md-12">
+				
+			</div>
+		</div><!--/.row-->
